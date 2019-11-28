ae5be7fd1367834b443cade96ca0b20b1055d742
vtorres96 <victorcdc96@gmail.com>
2019-11-28 20:27:59 -0300

    update: header

--- inc/header.php
+++ inc/header.php
@@ -14,7 +14,7 @@
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="subMenuTopo" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          Submenu
         </a>
         <div class="dropdown-menu" aria-labelledby="subMenuTopo">
           <a class="dropdown-item" href="#">Action</a>
