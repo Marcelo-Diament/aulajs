98101811929f392982241d6024ff66d5650820e8
vtorres96 <victorcdc96@gmail.com>
2019-11-28 19:49:22 -0300

    update: HTML inicial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Prática em Aula de JS</title>
+</head>
+<body>
+    <?php require_once("inc/head.php"); ?>
+    <?php require_once("inc/header.php"); ?>
+    <main id="content"></main>
+    <?php require_once("inc/footer.php"); ?>
+</body>
+</html>
