b3ed0995b1f5472f38377630338e4ab8c9c03b20
Marcelo-Diament <marcelodiament@gmail.com>
2019-12-05 00:59:08 -0300

    fix: ajustes de layout - na branch errada... rs...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,103 @@
 window.onload = function() {
 
-    sliderHome = document.getElementById('sliderHome')
-    console.log('\nsliderHome: '+sliderHome+'\n')
-    sliderHomeInner = sliderHome.children[1]
-    console.log('\nsliderHomeInner: '+sliderHomeInner+'\n')
-    sliderQuantidade = sliderHomeInner.childElementCount
-    console.log('\nsliderQuantidade: '+sliderQuantidade+'\n')
-    slides = sliderHomeInner.children
-    console.log('\nslides: '+slides+'\n')
-    console.log('\nslides[0]: '+slides[0]+'\n')
-    imagens = [
-        'http://images.unsplash.com/photo-1492305175278-3b3afaa2f31f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
-        'https://pics.freeartbackgrounds.com/fullhd/Country_Landscape_Background-198.jpg',
-        'https://wallpaperplay.com/walls/full/f/1/a/31817.jpg'
-    ]
-    titulos = ['Título 1', 'Título 2', 'Título 3']
-    legendas = ['Legenda 1', 'Legenda 2', 'Legenda 3']
+    // Variáveis
 
-    for(let i = 0; i < sliderQuantidade; i++){
-        slides[i].firstElementChild.setAttribute('src', imagens[i])
-        slides[i].firstElementChild.setAttribute('alt', titulos[i] + ' | '+ legendas[i])
-        slides[i].firstElementChild.nextElementSibling.children[0].innerText = titulos[i]
-        slides[i].firstElementChild.nextElementSibling.children[1].innerText = legendas[i]
-    }
+        // Header
+        header = document.getElementsByTagName('header')
+        console.log('\nheader: '+header+'\n')
+
+        // Container máximo dos slides (slider)
+        sliderHome = document.getElementById('sliderHome')
+        console.log('\nsliderHome: '+sliderHome+'\n')
+
+        // Container dos slides (inner)
+        sliderHomeInner = sliderHome.children[1]
+        console.log('\nsliderHomeInner: '+sliderHomeInner+'\n')
+
+        // Carousel Item (array com todos eles)
+        itensCarousel = document.querySelectorAll('.carousel-item')
+        console.log('\nitensCarousel: '+itensCarousel+'\n')
+        
+        // Quantidade de slides (no HTML)
+        sliderQuantidade = sliderHomeInner.childElementCount
+        console.log('\nsliderQuantidade: '+sliderQuantidade+'\n')
+
+        // Array com os slides
+        slides = sliderHomeInner.children
+        console.log('\nslides: '+slides+'\n')
+        console.log('\nslides[0]: '+slides[0]+'\n')
+
+        // Array com as imagens a serem inseridas nos slides
+        imagens = [
+            'assets/img/banner-topo-01.png',
+            'assets/img/banner-topo-02.png',
+            'assets/img/banner-topo-03.png'
+        ]
+
+        // Array com os títulos dos slides
+        titulos = ['Título 1', 'Título 2', 'Título 3']
+
+        // Arrya com as legendas dos slides
+        legendas = ['Legenda 1', 'Legenda 2', 'Legenda 3']
     
-    formulario = document.createElement('form')
-    formTitulo = document.createElement('h2')
-    formTituloContent = document.createTextNode('FORMULÁRIO')
-    formTitulo.appendChild(formTituloContent)
-    formulario.appendChild(formTitulo)
-    document.querySelector('main').appendChild(formulario)
-    formTitulo.style.color = 'red'
+    // /Variáveis
+
+    // Manipulando HTML
+
+        // Inserindo imagens, títulos e legendas aos slides com loop for
+        for(let i = 0; i < sliderQuantidade; i++){
+            
+            // Inserindo o atributo src
+            slides[i].firstElementChild.setAttribute('src', imagens[i])
+            
+            // Inserindo o atributo alt (usando o título e a legenda)
+            slides[i].firstElementChild.setAttribute('alt', titulos[i] + ' | '+ legendas[i])
+
+            // Atrelando estilo ao slide
+            slides[i].firstElementChild.style = 'height: 350px; max-height: 350px; width: auto;'
+
+            // Inserindo texto ao container do título
+            slides[i].firstElementChild.nextElementSibling.children[0].innerText = titulos[i]
+
+            // Inserindo texto ao container da legenda
+            slides[i].firstElementChild.nextElementSibling.children[1].innerText = legendas[i]
+
+        }
+        
+        
+        // Inserindo imagem de fundo ao container do slide con loop forEach
+        itensCarousel.forEach(element => {
+
+            /* Observação Importante: estamos fazendo um caminho 'torto': selecionamos um array de elementos para selecionar o pai deles, que é o mesmo.
+            ** Ou seja, poderíamos acessar diretamente o pai e não precisaríamos de um loop. Mas a ideia é mostrar como selecionar o pai e como usra o forEach.
+            ** Ah, e o motivo de selecionarmos o pai é para que o fundo fique sempre fixo (e a imagem do slide passe sobre o bg-img). */
+
+            // Repare que estamos acessando a propriedade do estilo e atrelando um valor a ela
+            element.parentElement.style.backgroundImage = 'url("assets/img/banner-topo-bg.png")'
+            // Atenção: as propriedades são escritas em camelCase (como backgroundColor ao invés de background-color)
+            
+            // Outra maneira é acessar apenas o estilo em si (style) e então declarar as propriedades e valores ao elemento selecionado
+            element.parentElement.style = 'background-image: url("assets/img/banner-topo-bg.png"); background-position: center; background-attachment: fixed'
+            /* Ainda que de formas distintas, por estarmos redeclarando novos valores à mesma propriedade dos mesmos objetos, acabamos por anular
+            ** a declaração anterior (do background-image). Por isso estamos declarando novamente. */
+
+        });
+
+        
+    // formulario = document.createElement('form')
+    // formTitulo = document.createElement('h2')
+    // formTituloContent = document.createTextNode('FORMULÁRIO')
+    // formTitulo.appendChild(formTituloContent)
+    // formulario.appendChild(formTitulo)
+    // document.querySelector('main').appendChild(formulario)
+    // formTitulo.style.color = 'red'
     
-    tarefa = document.querySelector('#tarefa')
-    tarefa.onblur = function(){
-        tarefa.innerText == ''
-        ? this.style.backgroundColor = 'red'
-        : this.style.backgroundColor = 'green'
-    }
+    // tarefa = document.querySelector('#tarefa')
+    // tarefa.onblur = function(){
+    //     tarefa.innerText == ''
+    //     ? this.style.backgroundColor = 'red'
+    //     : this.style.backgroundColor = 'green'
+    // }
     
 
 }
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 </head>
 <body>
     <?php require_once("inc/header.php"); ?>
-    <main id="content" class="container-fluid">
+    <main id="content" class="container-fluid px-0">
         <div id="sliderHome" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#sliderHome" data-slide-to="0" class="active"></li>
@@ -12,21 +12,21 @@
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="..." class="d-block w-100" alt="...">
+                    <img src="..." class="d-block" alt="...">
                     <div class="carousel-caption d-block">
                         <h5>First slide label</h5>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </div>
                 </div>
                 <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
+                <img src="..." class="d-block" alt="...">
                 <div class="carousel-caption d-block">
                     <h5>Second slide label</h5>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
                 </div>
                 <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
+                <img src="..." class="d-block" alt="...">
                 <div class="carousel-caption d-block">
                     <h5>Third slide label</h5>
                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
@@ -42,11 +42,19 @@
                 <span class="sr-only">Next</span>
             </a>
         </div>
-        <form action="">
-            <input type="text" name="tarefa" id="tarefa">
-            <button type="submit" onclick="adicionarTarefa(el)" id="adicionarTarefa">Enviar</button>
-        </form>
-        <ul id="lista"></ul>
+        <section>
+            <article id="tarefasFormArticle">
+                <h2>Insira suas tarefas</h2>
+                <form action="" method="post" id="tarefasForm">
+                    <input type="text" name="tarefa" id="tarefa">
+                    <button type="submit" onclick="adicionarTarefa(el)" id="adicionarTarefa">Enviar</button>
+                </form>
+            </article>
+            <article id="tarefasListaArticle">
+                <h2>Tarefas incluídas</h2>
+                <ul id="tarefasLista"></ul>
+            </article>
+        </section>
 
     </main>
     <?php require_once("inc/footer.php"); ?>
